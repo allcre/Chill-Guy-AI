5557297892d02912b6700d24274c1b5099d20350
Allison Cretel <allisoncretel@gmail.com>
2025-01-18 10:32:03 -0500

    redirect to google.com from new tab

--- chillguy/background.js
+++ chillguy/background.js
@@ -202,3 +202,10 @@ chrome.tabs.onCreated.addListener((tab) => {
     chrome.tabs.update(tab.id, { url: "https://www.google.com" });
   }
 });
+
+
+chrome.tabs.onCreated.addListener((tab) => {
+  if (tab.pendingUrl === "chrome://newtab/") {
+    chrome.tabs.update(tab.id, { url: "https://www.google.com" });
+  }
+});
